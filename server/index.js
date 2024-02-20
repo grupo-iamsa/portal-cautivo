36104f1cc5b2b812b8b2677a1a008408e266c243
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-02-20 16:20:51 -0600

    Se agrego manejo de errores al obtener el token y se cambio de modulos a cjs para usar archivos externos

--- server/index.js
+++ server/index.js
@@ -1,44 +1,28 @@
-import express  from "express";
-import axios from "axios";
-import https from "https"
-import path from "node:path";
-import { writeFileSync } from "node:fs"
+const express = require("express") ;
+const getToken = require("./helpers/getToken");
+
 
 const app = express()
 const PORT = process.env.PORT || 7000
 
-// Server Data
-const OMADA_IP = "192.168.100"
-const OMADA_PORT = "433"
-const OMADA_ID = "61c48dad6e3fdd80c79a1340bcf9817c"
+
+
+//Credentials Omada Controler
+
+const USER = 
 
 app.use(express.json())
-const agent = new https.Agent({
-  rejectUnauthorized: false
-});
 
 app.get("/api/login", async (req, res) => {
-  try {
-    const response = await axios.post(
-      'https://192.168.100.28:443/61c48dad6e3fdd80c79a1340bcf9817c/api/v2/hotspot/login',
-      {
-        name: "jasubi",
-        password: "jasubi"
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        httpsAgent: agent
-      }
-    );
-    const data = response.data;
-    console.log(data);
-    res.json(data);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  
+  const token = await getToken()
+
+  if(token.error){
+    res.json({error: token.error})
   }
+  
+  res.json(token)
+
 });
 
 app.listen(PORT, () =>{
