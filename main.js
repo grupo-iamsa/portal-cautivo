07ce334e27b34bff733b1da2c4c7da81b8f39f6f
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-02-20 14:03:24 -0600

    Se corrige la ID del dispositivo y se comienza la peticion del lado del servidor

--- main.js
+++ main.js
@@ -22,21 +22,23 @@ localStorage.setItem('eapData', JSON.stringify(eapData))
 
 const CONTROLLER_IP = "192.168.100.28"
 const PORT = "443"
-const CONTROLlER_ID = "61c48dad6e3fdd80c79a1340bcf9817"
+const CONTROLlER_ID = "61c48dad6e3fdd80c79a1340bcf9817c"
 
 const postData = {
-  OPERATOR_USERNAME : "usuario",
-  OPERATOR_PASSWORD : "usuario123456"
+  name : "jasubi",
+  password : "jasubi"
 }
 
 async function sendConection(){
-  const res = await fetch(`https://${CONTROLLER_IP}:${PORT}/${CONTROLlER_ID}/api/v2/hotspot/login`, {
+  const res = await fetch(`https://192.168.100.28:443/61c48dad6e3fdd80c79a1340bcf9817c/api/v2/hotspot/login`, {
     method: 'POST',
+    mode: 'same-origin',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(postData)
   })
+  console.log(JSON.stringify(postData))
   const data = res.json()
   console.log(data)
 }
--- server/index.js
+++ server/index.js
@@ -1,24 +1,45 @@
 import express  from "express";
+import axios from "axios";
+import https from "https"
 import path from "node:path";
 import { writeFileSync } from "node:fs"
 
 const app = express()
 const PORT = process.env.PORT || 7000
 
+// Server Data
+const OMADA_IP = "192.168.100"
+const OMADA_PORT = "433"
+const OMADA_ID = "61c48dad6e3fdd80c79a1340bcf9817c"
+
 app.use(express.json())
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
-app.get("/", (req, res) =>{
-  res.send("Hola")
-})
-
-app.get("/api", (req, res) =>{
-  // const { clientMac, apMac, gatewayMac, vid, ssidName, radioId, site, redirectUrl, t } = req.query
-  console.log(req.query)
-  const EAPInfo = req.query
-  
-  writeFileSync(`${path.join(process.cwd())}/clients/clientMac_${EAPInfo.clientMac}.json`, JSON.stringify(EAPInfo))
-  res.json(EAPInfo)
-})
+app.get("/api/login", async (req, res) => {
+  try {
+    const response = await axios.post(
+      'https://192.168.100.28:443/61c48dad6e3fdd80c79a1340bcf9817c/api/v2/hotspot/login',
+      {
+        name: "jasubi",
+        password: "jasubi"
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        httpsAgent: agent
+      }
+    );
+    const data = response.data;
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(PORT, () =>{
   console.log(`Escuchando en el puerto ${PORT}`)
